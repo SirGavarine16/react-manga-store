0c6470d77d2bc86d6f221b549e3e4785c7efcc1e
Daniel De la Cruz <danieldelagavarain.dev@gmail.com>
2022-12-04 13:20:51 -0700

    NavigationBar and FooterBar responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Helmet } from 'react-helmet';
 
 interface Props {
 }
@@ -6,6 +7,10 @@ interface Props {
 const Home: FC<Props> = () => {
     return(
         <>
+            <Helmet>
+                <title>Manga Store - Home</title>
+            </Helmet>
+
             <h1>Home Page</h1>
         </>
     );
