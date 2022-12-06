c390b258c26a36acce815edbb347051a5fb6952a
Daniel De la Cruz <danieldelagavarain.dev@gmail.com>
2022-12-06 13:51:03 -0700

    About Us page added

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,13 +1,39 @@
 import { FC } from 'react';
+import { Helmet } from 'react-helmet';
+
+import './../styles/aboutus.css';
 
 interface Props {
 }
 
 const AboutUs: FC<Props> = () => {
-    return(
-        <>
-            <h1>About Us Page</h1>
-        </>
+    return (
+        <main>
+            <Helmet>
+                <title>Manga Store - About Us</title>
+            </Helmet>
+
+            <h1 className='page-title'>About Us</h1>
+
+            <div className="aboutus-container">
+                <div>
+                    <picture>
+                        <source srcSet='/images/wotakoi.avif' type='image/avif' />
+                        <source srcSet='/images/wotakoi.webp' type='image/webp' />
+                        <img src='/images/wotakoi.png' alt='About Us - Wotaku ni Koi wa Muzukashii' className="aboutus-image" />
+                    </picture>
+                </div>
+                <div className="aboutus-content">
+                    <p>
+                        When the Manga Store was first planned, we were aiming to bring to all manga readers the experience that we most wanted: physical copies of our favorite stories including special editions available to everyone, everywhere.
+                    </p>
+                    <p>
+                        Now, the Manga Store is planning to bring even more than just manga. Weeb merchandise such as clothes, props, cosplays, anime figures and other products are coming soon. Hoping to bring more and more products in the future, we hope our store can give you a new way to experience your favorite manga.
+                    </p>
+                </div>
+            </div>
+
+        </main>
     );
 }
 
