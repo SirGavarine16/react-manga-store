650b894874423b8d786749cb014677a41c8a4d36
Daniel De la Cruz <danieldelagavarain.dev@gmail.com>
2022-12-07 06:00:56 -0700

    Store page base

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,13 +1,21 @@
 import { FC } from 'react';
+import { Helmet } from 'react-helmet';
+
+import './../styles/store.css';
 
 interface Props {
 }
 
 const Store: FC<Props> = () => {
     return(
-        <>
-            <h1>Store Page</h1>
-        </>
+        <main>
+            <Helmet>
+                <title>Manga Store - Store</title>
+            </Helmet>
+
+            <h1 className="page-title">Store</h1>
+
+        </main>
     );
 }
 
